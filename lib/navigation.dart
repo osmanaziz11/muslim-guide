d2afc2708117028a1b9ed38ed545509624430bda
Osman <osmanaziz012@gmail.com>
2023-01-08 18:13:46 +0500

    -

--- lib/navigation.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Navigation{
-  static GlobalKey<NavigatorState> appNavigation = GlobalKey();
-  static GlobalKey<NavigatorState> mainNavigation = GlobalKey();
-}
